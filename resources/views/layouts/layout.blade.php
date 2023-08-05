b62fd3a86d0088ba172e86a24c7a36dd4bef348b
neshika <stepanovaml@mail.ru>
2023-08-05 21:17:47 +0300

    require boostrap

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -47,7 +47,7 @@
     </nav>
 
 
-
+    
     @yield('content')
 
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,11 +4,18 @@
 <div class='container'>
     <div class="row">
         <div class="col-md-12">
+
+        <p><button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
             тут будет отображение картинки
-            <!--<img src="/{{$MyImg}}" alt="" class="img-thumbnail gallery-image">-->
+         </button></p>
+<div style="min-height: 120px;">
+  <div class="collapse collapse-horizontal" id="collapseWidthExample">
+    <div class="card card-body" style="width: 300px;">
+       <!--<img src="/{{$MyImg}}" alt="" class="img-thumbnail gallery-image">-->
             <img src="/{{$MyImg}}" class = "img-thumbnail" alt="" width=300>
-        </div>
     </div>
+  </div>
 </div>
+         
 
 @endsection
